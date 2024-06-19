506d956c67758f20903fd7a297ae7bd467876b31
Yi-HyeonJu <guswwn1925@gmail.com>
2024-06-19 13:47:12 +0900

    feat : test 파일 생성

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { vitest } from "vitest";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitest()],
   test: {
     globals: true,
     environment: "jsdom",
